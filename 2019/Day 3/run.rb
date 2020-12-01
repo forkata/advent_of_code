d5e938ae7289886c6b8365d975f98d8cea167dfe
Chris Todorov <ctodorov@juul.com>
2020-11-30 23:35:40 -0800

    Never even finished this one...

---
+++ 2019/Day 3/run.rb
@@ -0,0 +1,8 @@
+# @param input [String]
+# @return [Array<Array<String>>] a 2D representation of the grid
+def parse_input(input)
+
+end
+
+INPUT = "R8,U5,L5,D3\nU7,R6,D4,L4"
+puts parse_input(INPUT)
